61335c4217acb86a67e632ce71d7c750cfe00c34
hans <anselmeceril@gmail.com>
2025-05-23 11:27:29 +0200

    init project

---
+++ frontend/app/page.tsx
@@ -0,0 +1,13 @@
+import { Button } from "@/components/ui/button";
+
+export default function Home() {
+  return (
+    <div className="">
+      <h1> ResumeAI </h1>
+      <Button variant={"outline"} size={"lg"}>
+        {" "}
+        Hans{" "}
+      </Button>
+    </div>
+  );
+}
