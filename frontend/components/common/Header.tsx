bae491bb61e4b702c07d5ff7d2484e980d8156a2
hans <anselmeceril@gmail.com>
2025-05-23 12:56:51 +0200

    add header and Link compoenent

--- frontend/components/common/Header.tsx
+++ frontend/components/common/Header.tsx
@@ -1,22 +1,35 @@
-import Link from "next/link";
 import { FileText } from "lucide-react";
+import { Button } from "../ui/button";
+import NavLink from "./NavLink";
 
 const Header = () => {
+  const isLoggedIn = false;
   return (
     <nav className="container flex items-center justify-between py-4 lg:px-8 px-2 mx-auto">
       <div className="flex lg:flex-1">
-        <Link href="/" className="flex items-center gap-1 lg:gap-2 shrink-0">
+        <NavLink href="/" className="flex items-center gap-1 lg:gap-2 shrink-0">
           <FileText className="w-5 h-5 lg:w-8 lg:h-8 text-gray-900 hover:rotate-12 transform transition duration-200 ease-in-out" />
           <span className="font-extrabold lg:text-xl text-gray-900">
             ResumeAI
           </span>
-        </Link>
+        </NavLink>
       </div>
-      <div>
-        <Link href="/#pricing"> Princing</Link>
+      <div className="flex lg:justify-center gap-4 lg:gap-12 lg:items-center">
+        <NavLink href="/#pricing"> Princing</NavLink>
+        {isLoggedIn && <NavLink href="/#dashboard"> Your results </NavLink>}
       </div>
-      <div>
-        <Link href="/#signin"> SignIn</Link>
+      <div className="flex lg:justify-end lg:flex-1">
+        {isLoggedIn ? (
+          <div className="flex gap-2 items-center">
+            <NavLink href="/#upload">Upload PDF</NavLink>
+            <div> Pro </div>
+            <Button> User </Button>
+          </div>
+        ) : (
+          <div>
+            <NavLink href="/signin"> Sign In</NavLink>
+          </div>
+        )}
       </div>
     </nav>
   );
